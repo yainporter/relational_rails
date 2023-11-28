2affac441e951a1c2d382fa4cfaf35e7c3d19b87
Yain Porter <yainporter@gmail.com>
2023-11-27 18:12:10 -0700

    feat: Created parents#new form

---
+++ app/controllers/parents_controller.rb
@@ -0,0 +1,28 @@
+class ParentsController < ApplicationController
+  def index
+    @parents = ["Parent 1", "Parent 2"]
+  end
+
+  def new
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def show
+
+  end
+
+  def destroy
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get '/', to: 'welcome#index'
+  get '/parents', to: 'parents#index'
+  get '/parents/new', to: 'parents#new'
 end
